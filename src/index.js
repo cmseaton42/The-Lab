215f474a171bb17bf61d2a8a0f1b97dede27da16
Canaan Seaton <cmseaton42@gmail.com>
2017-12-20 10:27:19 -0600

    Added support for routing between demos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
 import DancingBalls from "./components/dancing-ball";
 
+const Home = () => {
+    return (
+        <div>
+            <img src="/TheLab.jpg" alt="The Lab" />
+        </div>
+    );
+};
+
 const App = () => {
     return (
-        <div className="lab-wrapper">
-            <DancingBalls />
-        </div>
+        <Router>
+            <div>
+                {/* Nav Menu */}
+                <div class="navbar">
+                    <div class="dropdown">
+                        <button class="dropbtn">
+                            Demos 
+                            <i class="fa fa-caret-down" />
+                        </button>
+                        <div class="dropdown-content">
+                            <Link to="/">Home</Link>
+                            <Link to="Dancing-Ball-Demo">Dancing Balls</Link>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Where the magic Happens */}
+                <div className="lab-wrapper">
+                    <Route exact path="/" component={Home} />
+                    <Route path="/Dancing-Ball-Demo" component={DancingBalls} />
+                </div>
+            </div>
+        </Router>
     );
 };
 
